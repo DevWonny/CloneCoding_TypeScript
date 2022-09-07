663f14d49c7c7a436a24d742a1ae1485b83f1afe
DevWonny <cjfdnjs1994@naver.com>
2022-09-07 22:42:19 +0900

    feat: Menu Section 퍼블리싱 진행중

--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from '../components/Header';
 import MainBanner from './MainBanner';
 import Brand from './Brand';
+import Menu from './Menu';
 
 const Main = () => {
   return (
@@ -10,6 +11,7 @@ const Main = () => {
       <Header />
       <MainBanner />
       <Brand />
+      <Menu />
     </>
   );
 };
--- src/views/Menu.tsx
+++ src/views/Menu.tsx
@@ -1,5 +1,48 @@
-import React from "react";
+import React from 'react';
 
-const Menu = () => {};
+import styled from 'styled-components';
+
+const Menu = () => {
+  return (
+    <MenuWrap>
+      <MenuHeaderContainer>
+        <h1>MENU</h1>
+        <span>피자선생의 도우가 맛있는 피자</span>
+      </MenuHeaderContainer>
+      <MenuContentsWrap></MenuContentsWrap>
+    </MenuWrap>
+  );
+};
 
 export default Menu;
+
+const MenuWrap = styled.div`
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  background: red;
+`;
+
+const MenuHeaderContainer = styled.div`
+  width: 100%;
+  height: 120px;
+  text-align: center;
+
+  & h1 {
+    font-size: 72px;
+    margin: 0;
+  }
+`;
+
+const MenuContentsWrap = styled.div`
+  width: 80%;
+  height: 620px;
+  position: relative;
+  left: 10%;
+  top: 100px;
+  background: gray;
+`;
+
+const MenuContentsContainer = styled.div``;
+
+const MenuContentsDiv = styled.div``;
