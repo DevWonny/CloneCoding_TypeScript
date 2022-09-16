7994eb1a32e5f54046236a5340a9dc3f428fadfb
DevWonny <cjfdnjs1994@naver.com>
2022-09-16 11:33:07 +0900

    feat: Loading Page 퍼블리싱 작업 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import "./App.css";
-import Main from "./views/Main";
+import React from 'react';
+import './App.css';
+import Main from './views/Main';
 
 function App() {
   return <Main />;
---
+++ src/components/Loading.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import styled from 'styled-components';
+import LoadingImage from '../assets/LoadingImage.svg';
+
+const Loading = () => {
+  return (
+    <LoadingWrap>
+      <LoadingContainer>
+        <img src={LoadingImage} alt="LoadingImage" />
+        <p>상담신청을 등록하는 중 입니다...</p>
+      </LoadingContainer>
+    </LoadingWrap>
+  );
+};
+
+export default Loading;
+
+const LoadingWrap = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: rgba(255, 255, 255, 0.9);
+`;
+
+const LoadingContainer = styled.div`
+  width: 100%;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  color: #75d3d1;
+  font-size: 25px;
+  font-weight: 900;
+  & img {
+    width: 150px;
+    height: 150px;
+  }
+`;
